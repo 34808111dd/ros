8b089483eccac0e21aa33f4c8d8fa638782ab6bf
34808111dd <0dd0495e8300d826@pop3.ru>
2014-09-05 16:54:58 +0400

    broken del contact button

--- static/clients.js
+++ static/clients.js
@@ -1,4 +1,10 @@
- var getCookie=function(name) {
+/* Wheels section
+ * getCookie, document.ready and window.ready
+ */
+
+//Get cookie by name
+//Used in POST queries to send csrftoken value.
+function getCookie(name) {
         var cookieValue = null;
         if (document.cookie && document.cookie != '') {
             var cookies = document.cookie.split(';');
@@ -12,242 +18,197 @@
             }
         }
         return cookieValue;
-    };
-
-
-var place_all_clients = function(json_clients)
-{
-
 };
 
+/*
+ *Data load section
+ */
 
-/*var get_clients = function()
-{
-$.post( "/rnr/clients_all", {'csrfmiddlewaretoken':getCookie('csrftoken')},
-	function( data ) {
-	  alert( "Data Loaded: " + data );
+function LoadClients() {
+	//Clear existing data
+	$('#existing_clients tbody').html("");
+	//Get data from server in json
+	$.getJSON( "/rnr/clients_all", function(data) {
+		//for all clients in data
+		 
+		$.each( data, function( index, element ) {
+			var items = [];
+			var row = ""
+			row =row.concat("<tr>");
+			row =row.concat("<td id=" +  element.client_slug + ">");
+			row =row.concat(element.attribs.client_name);
+			row =row.concat("</td>");
+			row =row.concat("<td id=" +  element.attribs.client_language__slug + ">");
+			row =row.concat(element.attribs.client_language__language_name);
+			row =row.concat("</td>");
+			row =row.concat("<td>");
+			//for all contacts in client
+			$.each(element.contacts, function(index, element) {
+			var	btn = '<button ' + 'id="' + element.slug +'" class="btn_del_contact" type="button"></button>'
+				row =row.concat("<div class=contact_email id='"+element.slug + "'>" + element.contact_email + btn + "</div>");
+			});
+			row =row.concat("</tr>");
+			$('#existing_clients tbody').append(row);
+		});
+		Load_btn_del_contact();
+		
 	})
-.done( function( data ) {alert( "Data Loaded: " + data );});
-};
-//;
-
-var gg = function()
-{
-$.getJSON('/rnr/clients_all', { get_param: 'value' }, function(data) {
-    $.each(data, function(index, element) {
-        $('body').append($('<div>', {
-            text: element.name
-        }));
-    });
-});
+	.done(Load_btn_del_contact());
 };
 
-var ggg = function()
-{
- 
-  $.post("/rnr/clients_all", {
-    ajax: "true",
-    action: "",
-    'csrfmiddlewaretoken':getCookie('csrftoken')
-}).always(function(data){
-      alert(data);
-      var items = [];
-      $.each( data, function( key, val ) {
-      alert(key, val);
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
-});
-});
+function blah(){
+	var d = $(this).attr("id");
+	alert(d);
 };
-
-
-*/
 
 var LoadWorkTypes = function() {
-	$('#work_types').html("");
-	
+	$('#existing_work_types').html("");
 	$.getJSON( "/rnr/get_work_types_json", function(data){
-	      $.each( data, function( index, element ){
-	    	  
-	    	  $('#work_types').append('<p id=' + element.slug + ' class="ui-widget work_type">'+ element.worktype_name +'</p>')
-		     // alert(element.slug + " " +  );
-		    });
-});
-};
-
-
-var LoadClients = function() {
-$('#mytable tbody').html("");
-$.getJSON( "/rnr/clients_all", function(data) {
-table = "";//"<table id='client_table' class='ui-widget ui-widget-content'>"
-      $.each( data, function( index, element ) {
-	var items = [];
-	row = ""
-	row =row.concat("<tr>");
-	
-	row =row.concat("<td id=" +  element.client_slug + ">");
-	row =row.concat(element.attribs.client_name);
-	row =row.concat("</td>");
-	
-	row =row.concat("<td id=" +  element.attribs.client_language__slug + ">");
-	row =row.concat(element.attribs.client_language__language_name);
-	row =row.concat("</td>");
-	row =row.concat("<td>");
-	
-	$.each(element.contacts, function(index, element) {
-		btn = '<button ' + 'id="' + element.slug +'" role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only del_contact_button" type="button"><span class="ui-button-icon-primary ui-icon ui-icon-closethick"></span></button>'
-		
-	    row =row.concat("<div class=contact_email id='"+element.slug + "'>" + element.contact_email + btn + "</div>");
+		$.each( data, function( index, element ){
+			$('#existing_work_types').append('<p id=' + element.slug + ' class="ui-widget work_type">'+ element.worktype_name +'</p>')
+		});
 	});
-	row =row.concat("</tr>");
-	table = table.concat(row);
-});
-      //table = table.concat("</table>");
-      $('#mytable tbody').append(table);
-      
-});
 };
 
-
-var LoadDelDialog = function() {
-$( "#dlg_confirm_del_contact" ).dialog({
-	autoOpen: false,
-	resizable: false,
-	height:180,
-	modal: true,
-	buttons: {
-"Delete Contact": function() {
-$( this ).dialog( "close" );
-return true;
-},
-Cancel: function() {
-$( this ).dialog( "close" );
-return false;
-}
-}
-});
-};
-
-
-
-
-
-
-
-var CreateUser = function(){
-	//send form data as ajax
-	//alert("sended");
-	var client_name = $( "#client_name" ).val();
-	var client_language = $( "#client_language" ).val();
-	var client_emails = $( "#client_emails" ).val();
-	//alert (client_emails.split(","));
-	//add_new_client
-	
-/*	
-	$.ajax("/rnr/add_new_client", {
-	    data : JSON.stringify(myJSObject),
-	    contentType : 'application/json',
-	    type : 'POST',
-	});
-	
-	
-	
-	 $.post("/rnr/clients_all", {
-    ajax: "true",
-    action: "",
-    'csrfmiddlewaretoken':getCookie('csrftoken')
-	
-	
-*/	
-	 var posting = $.post( "/rnr/add_new_client", {
-		 ajax: "true",
-		 "client_name": client_name,
-		 "client_language" : client_language,
-		 "client_emails":client_emails,
-		 'csrfmiddlewaretoken':getCookie('csrftoken'),
-	 });
-	// Put the results in a div
-	posting.done(function( data ) {
-	//alert(data);
-	if (data === "OK"){
-		$("#client_add_errors").html("");
-		 $('#frm_add_client').trigger("reset");
-		 $('#dlg_add_user').dialog("close");
-		 LoadClients();
-		return true;
-	}
-	else{
-		$("#client_add_errors").html(data);
-		return false;
-	};	
-	});
-	
-	
-	//alert(client_name + client_language +client_emails);
-//	$(this).dialog.("close");
-};
-
-
+//On document ready - load data, apply styling
+//Load data, add jQuery UI to dialog elements
 $(document).ready(function(){
 	LoadClients();
 	LoadWorkTypes();
+	Load_ui_elements();
+	//Load_btn_del_contact();
 	
-	LoadDelDialog();
-//	LoadWorkTypes();
-	$( "#dlg_add_user" ).dialog({
+});
+
+//$(window).ready(Load_btn_del_contact());
+
+function Load_btn_del_contact(){
+	//$( ".selector" ).button;
+	$( ".btn_del_contact" ).button({ icons: { primary: "ui-icon-trash" } });
+	$( ".btn_del_contact" ).click(function() {
+       var cid = $(this).attr("id");
+       
+       var cont = $(this).parent().text();
+       $("#del_contact").text(cont);
+      ans = $( "#dlg_confirm_del_contact" ).dialog( "open" );
+      alert(ans);
+       if (ans===true){
+       	$("div #"+cid).remove();
+       };
+    });
+	
+}
+
+
+//Add new client dialog
+function Load_dlg_add_client(){
+	$( "#dlg_add_client" ).dialog({
         autoOpen: false,
         height: 400,
         width: 450,
         modal: true,
         buttons: {
-        	"Create User": function(){CreateUser();        	
-        	},//$(this).dialog("close");},
-        	Cancel: function() {$(this).dialog("close");
-        	$('#frm_add_client').trigger("reset");
-        	$("#client_add_errors").html("");}
-				
-         },
-         title: "Add new Client",
-//         position: {
-//            my: "left center",
-//            at: "left center"
-//         },
-        
+        			"Create User": function(){CreateClient();},
+        			Cancel: function() {
+        				$(this).dialog("close");
+        				$('#frm_add_client').trigger("reset");
+        				$("#dlg_add_client_errors").html("");
+        				}
+         		},
+         title: "Add new Client",        
         });
+};
+
+
+//Delete contact confirm dialog
+function Load_dlg_del_contact(){
+	$( "#dlg_confirm_del_contact" ).dialog({
+		autoOpen: false,
+		resizable: false,
+		height:180,
+		modal: true,
+		buttons: {
+			"Delete Contact": function() {
+				$( this ).dialog( "close" );
+				return true;
+			},
+			Cancel: function() {
+				$( this ).dialog( "close" );
+				return false;
+			}
+		}
+	});
+};
+
+
+//Load UI elements with jQuery styling.
+//Loads after data and dialogs are loaded - on window ready
+function Load_ui_elements(){
 	
-	$( "#opener" ).button();
-    $( "#opener" ).click(function() {
-        $( "#dlg_add_user" ).dialog( "open" );
-     });
-    
-    
-    
-    $( "#opn_confirm_del" ).button();
-    $( "#opn_confirm_del" ).click(function() {
-        $( "#dlg_confirm_del_contact" ).dialog( "open" );
-     });
-    
-    
-	});
+	Load_dlg_add_client();
+	Load_dlg_del_contact();
+
+	
+	//Add Create client button
+	$( "#btn_create_client" ).button();
+	//bind Create client dialog on click
+	$( "#btn_create_client" ).click(function() {
+	$( "#dlg_add_client" ).dialog( "open" );});
+	//
+	$( "#client_language" ).selectmenu();
+	
+	//Add button styling to delete contact button
+//	$( ".btn_del_contact" ).button();
+	//bind click on btn_del_contact to open delete contact dialog
+//	$( ".btn_del_contact" ).click(function() {
+//	$( "#dlg_confirm_del_contact" ).dialog( "open" );});
+
+};
 
 
-$(window).load(function() {
-	 // executes when complete page is fully loaded, including all frames, objects and images
-	 //alert("window is loaded");
-	 
-	$( "#mytable tbody button" ).click(function() {
-  	  //$( "#dlg_confirm_del_contact" ).dialog( "open" );
-        var cid = $(this).attr("id");
-        var cont = $(this).parent().text();
-        //alert(cont);
-        $("#del_contact").text(cont);
-        ans = $( "#dlg_confirm_del_contact" ).dialog( "open" );
-        alert(ans);
-        if (ans===true){
-        	$("div #"+cid).remove();
-        };
-        //alert(k);
-        
-        
-     });
-	 
+function DelContact(contact_slug){
+	alert(contact_slug);
+};
+
+
+/* Content management functions
+ * for clients:
+ * - Get all clients via ajax and json
+ * - Load data in #existing_clients table tbody
+ */
+
+function CreateClient(){
+	//send form data as ajax
+	//alert("sended");
+	var client_name = $( "#client_name" ).val();
+	var client_language = $( "#client_language" ).val();
+	var client_emails = $( "#client_emails" ).val();
+	var posting = $.post( "/rnr/add_new_client", {
+		ajax: "true",
+		"client_name": client_name,
+		"client_language" : client_language,
+		"client_emails":client_emails,
+		'csrfmiddlewaretoken':getCookie('csrftoken'),
 	});
+	// Put the results in a div
+	posting.done(function( data ) {
+		//alert(data);
+		if (data === "OK"){
+			$("#dlg_add_client_errors").html("");
+			$('#frm_add_client').trigger("reset");
+			$('#dlg_add_client').dialog("close");
+			LoadClients();
+			return true;
+		}
+		else{
+			$("#dlg_add_client_errors").html(data);
+			return false;
+		};	
+	});
+};
+
+
+
+
+
+
